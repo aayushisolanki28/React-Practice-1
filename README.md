bd2886e4390e688d31a513ca78a0038fb5b672fb
aayushisolanki28 <36412316+aayushisolanki28@users.noreply.github.com>
2023-06-19 10:14:00 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# React-Practice-1
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/stackblitz-starters-r9seug)
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css";
+import Header from './components/Header';
+import AddContact from './components/AddContact'
+import ContactList from './components/ContactList'
+
+export default function App() {
+  return (
+    <div>
+     <Header/>
+    </div>
+  );
+}
